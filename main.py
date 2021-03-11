e92080e7d0df589ffffe91af12d9afabc3fc91a6
Chris Bochicchio <chris.bochicchio@gmail.com>
2021-03-11 14:15:16 -0500

    .

--- main.py
+++ main.py
@@ -1,39 +1,21 @@
 # function to import excel sheets
 def getSheet(path, sheet_name=None):
     df = pd.read_excel(path, sheet_name, header=None)
-
     return df
 
 
-# function for finding a string inside of a pandas dataframe
-def findString(df, string, startWith=False):
-
-    if startWith == False:
-        filt = df == string
-        [i, j] = np.where(filt == True)
-
-###
-#    if startWith == True:
-#        i = list([])
-#        j = list([])
-#        for col in df.columns:
-#            try:
-#                temp = df[col].str.startswith(string)
-#            except AttributeError:
-#                print("Found a blank column: #", col)
-#            else:
-#                ii = list(np.where(temp == True))
-#                if np.sum(ii) > 0:
-#                    i.append(ii)
-#
-#    i = list(i)
-    return i, j if np.size(i) == 1 else print(
-        "Error: More than one "
-        + string
-        + " found in "
-        + sheet_name
-        + " or not found at all."
-    )
+def find_string(df, string):
+    ref = pd.DataFrame()
+    for c in df.columns:
+        try:
+            filt = pd.DataFrame(df[c].str.startswith(string))
+        except AttributeError:
+            print("Found a blank column: #", c + 1)
+        else:
+            ref = pd.concat([ref, filt], axis=1)
+    ref = np.where(ref == True)
+    ref = list([startRef[0][0], startRef[1][0]])
+    return ref
 
 
 # -----------------------------------------
@@ -44,31 +26,35 @@ import pandas as pd
 path = "magic_resave.xlsx"
 sheet_name = "July11"
 sampleNameString = "SAMP WT"
-sizeFractionString = "SIZE (MM)"
-
+sizeFractionStartString = "SIZE (MM)"
+sizeFractionEndString = "<"
 # load sheet
 df = getSheet(path, sheet_name)
 
 # Find the index of a string to act as a reference point in
 # case of non-standard sheet layout. Default = 'SAMP WT'
-[i, j] = findString(df, sampleNameString)
 
+sampRef = find_string(df, sampleNameString)
 
 # index of site names (refrenced from string above)
-SITENAME = df.iloc[i - 2]
-SITENAME = SITENAME.dropna(1)
+SITENAME = df.iloc[sampRef[0] - 3]
+# SITENAME = SITENAME.dropna(1)
 
 # index of site subtitles (refrenced from string above)
-SUBNAME = df.iloc[i - 1]
-SUBNAME = SUBNAME.dropna(1)
+SUBNAME = df.iloc[sampRef[0] - 2]
+# SUBNAME = SUBNAME.dropna(1)
 
 # find size catagory info
-[i, j] = findString(df, sizeFractionString)
+# size fraction start...
+startRef = find_string(df, sizeFractionStartString)
+startRef[0] = startRef[0] + 1
+largestSizeFraction = df.iloc[startRef[0], startRef[1]]
+# size fraction end...
+endRef = find_string(df, sizeFractionEndString)
+smallestSizeFraction = df.iloc[endRef[0], endRef[1]]
 
-largestSizeFraction = i + 1
+if startRef[1][0] == endRef[1][0]:
 
-SIZEFRACTION = df.iloc[:, j]
-SIZEFRACTION = SIZEFRACTION.dropna(0)
-
-SIZEFRACTION = SIZEFRACTION.squeeze()
-smallestSizeFraction = SIZECATAGORY.str.startswith("<")
+    binIndex = np.arange(startRef[0][0], endRef[0][0] + 1)
+    binIndex = binIndex.tolist()
+    BINFRACTION = binIndex
